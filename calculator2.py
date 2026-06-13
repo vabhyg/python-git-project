e53efb98bfcbdb05916392d448d1764738c4a411
Vaibhav <garg.vbhav@gmail.com>
2026-06-12 23:34:58 -0400

    feat: added subtract function

--- calculator2.py
+++ calculator2.py
@@ -1,5 +1,15 @@
+# calculator.py
 def add(a, b):
     return a + b
 
+# NEW CODE HERE
+def subtract(a, b):
+    return a - b
+# END NEW CODE
+
 if __name__ == "__main__":
-    print(f"Addition Result: {add(10, 5)}")
+    result = add(5, 3)
+    print(f"The result of addition is: {result}")
+    # NEW CODE HERE
+    result_sub = subtract(10, 4)
+    print(f"The result of subtraction is: {result_sub}")
